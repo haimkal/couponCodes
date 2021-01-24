4601fcda31c4e950eefaca4eadca1822b49ca589
Haim Kalvo <you@example.com>
2021-01-24 14:56:47 +0200

    Mongo DB REST + Bonus

--- src/controllers/coupon.js
+++ src/controllers/coupon.js
@@ -1,6 +1,7 @@
 
 const {db} = require('../db.js');
 const {ObjectId } = require('mongodb');
+const { report } = require('../middlewares/couponroutes.js');
 class Coupon {
 
     static create(req, res) {
@@ -26,6 +27,10 @@ class Coupon {
             .toArray()
             .then((coupons) => {
                 res.send(coupons);
+                })
+            .catch((e) => {
+                console.log(e);
+                res.sendStatus(500);
             });
     }
 
@@ -38,11 +43,88 @@ class Coupon {
                 return;
             }
             res.send(coupon);
+            })
+        .catch((e) => {
+            console.log(e);
+            res.sendStatus(500);
         });
     }
 
     
+    static edit (req, res) {
+	db().collection('coupons')
+	.updateOne(
+		{_id: ObjectId(req.params.id)},
+		{$set: req.body}
+		)
+		.then((report)=> {
+			if(report.matchedCount === 0){
+				res.sendStatus(404);
+				return;
+			}
+			res.status(200).send('coupon has been edited');    
+            })
+        .catch((e) => {
+            console.log(e);
+            res.sendStatus(500);
+            });
+    }
+    
+    static delete (req, res){
+        db().collection('coupons')
+		.deleteOne({_id: ObjectId(req.params.id)})
+		.then((report)=> {
+			if(report.deletedCount === 0){
+				res.sendStatus(404); 
+				return;
+			}
+			res.sendStatus(204);
+            })
+        .catch((e) => {
+            console.log(e);
+            res.sendStatus(500);
+            });
+    }
 
-}
+    static redeem (req, res){
+        db().collection('coupons')
+        .updateOne(	
+            {_id: ObjectId(req.params.id)},
+            {$set: {isRedeem: true} }
+                    )
+        .then ((report)=>{
+            if(report.modifiedCount ===0){
+                res.status(400).send('The coupon has been already redeemed');
+                return;
+            }
+            res.status(200).send('The coupon has been redeemed');
+            })
+        .catch((e) => {
+            console.log(e);
+            res.sendStatus(500);
+            });
+    }
+
+    static search (req,res) {
+        db().collection('coupons')
+        .findOne({code: req.params.code})
+        .then(code => {
+            if (!code) {
+                res.status(400).send("this coupon code doesn't exist");
+                return;
+            }
+            res.status(200).send(`coupon code ${req.params.code} exists` );
+            })
+        .catch((e) => {
+            console.log(e);
+            res.sendStatus(500);
+            });
+    }
+
+        
+
+
+
+}   
 
 module.exports = Coupon;
--- src/middlewares/couponroutes.js
+++ src/middlewares/couponroutes.js
@@ -5,7 +5,9 @@ const Coupon = require('../controllers/coupon')
 
 router.put('/', Coupon.create);
 router.get('/', Coupon.getAll);
-router.get('/:id', Coupon.get )
-
-
+router.get('/:id', Coupon.get );
+router.post('/:id', Coupon.edit);
+router.delete('/:id', Coupon.delete);
+router.post('/:id/redeem', Coupon.redeem); 
+router.get('/search/:code', Coupon.search)
 module.exports = router;
